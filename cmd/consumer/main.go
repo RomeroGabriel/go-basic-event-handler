ce102ac633d4321c8d32b202bec669e9e4e2e21e
RomeroGabriel <gabrielromerods@hotmail.com>
2023-12-31 14:42:52 -0300

    started with rabbitmq

---
+++ cmd/consumer/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/RomeroGabriel/go-basic-event-handler/pkg/rabbitmq"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	ch := rabbitmq.OpenChannel()
+	defer ch.Close()
+
+	msgs := make(chan amqp.Delivery)
+	go rabbitmq.Consume(ch, msgs)
+	for msg := range msgs {
+		fmt.Println(string(msg.Body))
+		msg.Ack(false)
+	}
+}
